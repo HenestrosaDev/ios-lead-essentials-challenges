5b68b2783f1567774e4a1c02d273da03f147f409
HenestrosaConH <henestrosaconh@gmail.com>
2023-05-24 16:37:46 +0200

    Add `load` implementation

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,6 +19,16 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		fatalError("Must be implemented")
+		client.get(from: url) { [weak self] result in
+			guard self != nil else { return }
+
+			switch result {
+			case let .success((data, response)):
+				completion(FeedImagesMapper.map(data, from: response))
+
+			case .failure:
+				completion(.failure(Error.connectivity))
+			}
+		}
 	}
 }
